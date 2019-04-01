ace1042b3056c525adc3c6b50f12507fa005c04a
blanklist <joehineriv@gmail.com>
2019-04-01 12:09:15 -0700

    typo fix

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -88,7 +88,7 @@ class WaypointUpdater(object):
         self.base_waypoints = waypoints
         if not self.waypoints_2d:
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
-            self.waypoint_tree = KDTree(self.wapoint_2d)
+            self.waypoint_tree = KDTree(self.waypoint_2d)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
