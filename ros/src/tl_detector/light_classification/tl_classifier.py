0963d65934d46ded2e2b454ead4c1088902083e5
blanklist <joehineriv@gmail.com>
2019-04-14 16:56:46 -0700

    adjust red boundries

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -15,25 +15,22 @@ class TLClassifier(object):
         output = image.copy()
         red = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        lower_red = np.array([0,50,50])
+        lower_red = np.array([0,100,100])
         upper_red = np.array([10,255,255])
         red1 = cv2.inRange(red, lower_red , upper_red)
 
-        lower_red = np.array([170,50,50])
-        upper_red = np.array([180,255,255])
+        lower_red = np.array([160,100,100])
+        upper_red = np.array([179,255,255])
         red2 = cv2.inRange(red, lower_red , upper_red)
 
-        converted_img = cv2.addWeighted(red1, 1.0, red2, 1.0, 0.0)
+        converted_img = cv2.addWeighted(red1, 1, red2, 1, 0)
 
         blur_img = cv2.GaussianBlur(converted_img,(15,15),0)
-        # blur_img = cv2.GaussianBlur(red,(15,15),0)
 
-        circles = cv2.HoughCircles(blur_img,cv2.HOUGH_GRADIENT,0.5,41, param1=70,param2=30,minRadius=5,maxRadius=150)
-        # circles = cv2.HoughCircles(blur_img,cv2.HOUGH_GRADIENT,1,60, param1=100,param2=50,minRadius=8,maxRadius=80)
-        # circles = cv2.HoughCircles(blur_img, cv2.HOUGH_GRADIENT, 1, 80, param1=100, param2=50)
-
+        circles = cv2.HoughCircles(blur_img,cv2.HOUGH_GRADIENT,0.5,40, param1=70,param2=30,minRadius=5,maxRadius=150)
+  
         colorized_blur_img = cv2.cvtColor(blur_img, cv2.COLOR_GRAY2BGR)
-        
+
         if circles is not None:
             result = TrafficLight.RED
 
