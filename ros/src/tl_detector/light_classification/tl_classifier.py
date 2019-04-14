62b77d50aca727bd7e593eed2c9d8aabb2bcbdc5
blanklist <joehineriv@gmail.com>
2019-04-13 20:42:19 -0700

    attempt to save images

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import numpy as np
 from styx_msgs.msg import TrafficLight
 
@@ -8,50 +9,56 @@ class TLClassifier(object):
         pass
 
     def get_classification(self, image):
-        """Determines the color of the traffic light in the image
 
-        Args:
-            image (cv::Mat): image containing the traffic light
-
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        #TODO implement light color prediction
-        # return TrafficLight.UNKNOWN
-        # EXPERIMENT
-        result = TrafficLight.UNKNOWN
         output = image.copy()
-        red = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        cv2.imwrite('img_results/image.jpg', output)
 
 
-        # lower_red = np.array([0,50,50])
-        # upper_red = np.array([10,255,255])
-        # red1 = cv2.inRange(red, lower_red , upper_red)
+
+        return result, output
 
 
-        # lower_red = np.array([170,50,50])
-        # upper_red = np.array([180,255,255])
-        # red2 = cv2.inRange(red, lower_red , upper_red)
 
-        # converted_img = cv2.addWeighted(red1, 1.0, red2, 1.0, 0.0)
 
-        # blur_img = cv2.GaussianBlur(converted_img,(15,15),0)
 
-        blur_img = cv2.GaussianBlur(red, (15, 15), 0)
 
-        #edges = cv2.Canny(imgray,thresh,thresh*3)
 
-        circles = cv2.HoughCircles(blur_img,cv2.HOUGH_GRADIENT,0.5,41, param1=70,param2=30,minRadius=5,maxRadius=150)
 
-        found = False 
-        if circles is not None:
-            result = TrafficLight.RED
-        #    for i in circles[0,:3]:
-        #        cv2.circle(output,(i[0],i[1]),maxRadius,(255, 100, 100),2)
+
+        # EXPERIMENT
+        # result = TrafficLight.UNKNOWN
+        # output = image.copy()
+        # red = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+
+        # # lower_red = np.array([0,50,50])
+        # # upper_red = np.array([10,255,255])
+        # # red1 = cv2.inRange(red, lower_red , upper_red)
+
+
+        # # lower_red = np.array([170,50,50])
+        # # upper_red = np.array([180,255,255])
+        # # red2 = cv2.inRange(red, lower_red , upper_red)
+
+        # # converted_img = cv2.addWeighted(red1, 1.0, red2, 1.0, 0.0)
+
+        # # blur_img = cv2.GaussianBlur(converted_img,(15,15),0)
+
+        # blur_img = cv2.GaussianBlur(red, (15, 15), 0)
+
+        # #edges = cv2.Canny(imgray,thresh,thresh*3)
+
+        # circles = cv2.HoughCircles(blur_img,cv2.HOUGH_GRADIENT,0.5,41, param1=70,param2=30,minRadius=5,maxRadius=150)
+
+        # found = False 
+        # if circles is not None:
+        #     result = TrafficLight.RED
+        # #    for i in circles[0,:3]:
+        # #        cv2.circle(output,(i[0],i[1]),maxRadius,(255, 100, 100),2)
       
         
-        #need to include more image, so ignore other colors
-        #green may be trees.  Just look for red lights
-        #if red_area > 40:
-        return result, output
+        # #need to include more image, so ignore other colors
+        # #green may be trees.  Just look for red lights
+        # #if red_area > 40:
+        # return result, output
